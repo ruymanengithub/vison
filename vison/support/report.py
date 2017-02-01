3dfd485b522d831b1896416efd54585e5b19bd27
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2017-02-01 15:21:13 +0000

    improvements in reporting: report.py and Latex files

--- vison/support/report.py
+++ vison/support/report.py
@@ -19,6 +19,12 @@ import os
 from vison.pipe.lib import get_time_tag
 from latex import generate_header, generate_preamble
 from vison import data as visondata
+
+from astropy import table as astable
+from astropy.io import ascii
+
+import tempfile
+import string as st
 # END IMPORT
 
 
@@ -65,32 +71,94 @@ class Section(Content):
         
     def generate_Latex(self):
         """ """
-        tex = ['\%s{%s}' % (self.type,self.Title)]
+        tex = ['\%s{%s}' % (self.type,self.Title),'\n']
         return tex
 
 class Figure(Content):
     """ """
-    def __init__(self,figpath,caption=''):
+    def __init__(self,figpath,textfraction=0.7,caption=None,label=None):
         
         self.type = 'figure'
+        self.figpath = figpath
+        self.textfraction = textfraction
         self.caption = caption
+        self.label = None
+        
         
     def generate_Latex(self):
-        """PENDING"""
-        tex = ['']
+        """Generates LaTeX as list of strings."""
+        
+        
+        tex_raw = ['\\begin{figure}[!htb]',
+        '\centering',
+        '\includegraphics[width=__PLACEHOLDER__\\textwidth]{__PLACEHOLDER__}',
+        '\\vspace{-1mm}',
+        '\end{figure}']
+        
+        substitutes = [self.textfraction,self.figpath]
+        
+        if self.caption is not None:
+            tex_raw.insert(-1,'\caption{__PLACEHOLDER__}')
+            substitutes.append(self.caption)
+        
+        if self.label is not None:
+            tex_raw.insert(-1,'\label{__PLACEHOLDER__}')
+            substitutes.append(self.label)
+        
+        texjoint = st.join(tex_raw,'JOIN_LINES')
+        texjoint = texjoint.replace('__PLACEHOLDER__','%s')
+        texjoint = texjoint % tuple(substitutes)
+        
+        tex = st.split(texjoint,'JOIN_LINES')
+        tex += ['\n'] # just improves legigibility of .tex
+        
         return tex
 
 
 class Table(Content):
-    """ """
-    def __init__(self,tableDict,caption=''):
+    """ 
+    PENDING:
+        - adjust width of table to texwidth:
+            \resizebox{\textwidth}{!}{
+            ...
+            \end{tabular}}
+        - include option to rotate table to show in landscape
+    
+    """
+    def __init__(self,tableDict,formats=dict(),names=[],caption=None):
+        
+        table = astable.Table(data=tableDict,names=names)
         
         self.type = 'table'
+        self.table = table
+        self.formatDict = formats
         self.caption = caption
         
-    def generate_Latex(self):
-        """PENDING"""
-        tex = ['']
+    def generate_Latex(self,):
+        """Generates LaTeX as list of strings."""
+        
+        table = self.table
+        formats = self.formatDict
+        
+        tf = tempfile.TemporaryFile()
+        
+        ascii.write(table,output=tf,formats=formats,Writer=ascii.Latex)
+        
+        tf.seek(0)
+        tex = tf.readlines()
+        tf.close()
+        
+        if self.caption is not None:
+            captiontex = '\caption{%s}\n' % self.caption
+            tex.insert(-1,captiontex)
+        
+        tex = [item[0:-1] for item in tex] # removes ending \n
+        
+        tex += ['\n'] # just improves legigibility of .tex
+        
+        
+        tex = [st.replace(item,'_','\_') for item in tex]
+        
         return tex
     
     
@@ -103,7 +171,13 @@ class Text(Content):
         
     def generate_Latex(self):
         """ """
-        tex = self.text
+        if isinstance(self.text,str):
+            tex = [self.text]
+        elif isinstance(self.text,list):
+            tex = self.text
+        
+        tex += ['\n'] # just improves legigibility of .tex
+        
         return tex
 
 class Report(object):
@@ -140,7 +214,7 @@ class Report(object):
         Texbody = generate_preamble(model,test,custodian)
         
         for item in self.Contents:            
-            Texbody += item.generate_Texbody()
+            Texbody += item.generate_Latex()
                         
         self.Texbody = Texbody
     
@@ -149,23 +223,35 @@ class Report(object):
         self.Contents.append(Section(Title,level))
         
         
-    def add_Figure(self,figpath,caption=''):
+    def add_Figure(self,figpath,texfraction=0.7,caption=None,label=None):
         """ """
-        self.Contents.append(Figure(figpath,caption))
+        self.Contents.append(Figure(figpath,texfraction,caption,label))
         
-    def add_Table(self,tableDict,caption=''):
+    def add_Table(self,tableDict,formats=dict(),names=[],caption=''):
         """ """
-        self.Contents.append(Table(tableDict,caption))
+        # tableDict,formats=dict(),names=[],caption=None
+        self.Contents.append(Table(tableDict,formats,names,caption))
         
     def add_text(self,text):
         """ """
         self.Contents.append(Text(text))
     
     
+    def doreport(self,reportname,cleanafter=False):
+        
+        self.generate_Header()
+        self.generate_Texbody()
+        outfiles = self.writeto(reportname,cleanafter)
+        return outfiles
+        
+    
     def writeto(self,fileroot,cleanafter=False):
         """Writes pdf with contents"""
         
         fileLaTex = '%s.tex' % fileroot
+        
+        if os.path.exists(fileLaTex):
+            os.system('rm %s' % fileLaTex)
         
         self.writeLaTex(fileLaTex)
         outfiles = self.compileLaTex2pdf(fileroot,cleanafter=cleanafter)
@@ -187,9 +273,11 @@ class Report(object):
         
         EuclidViscls = 'EuclidVIS.cls'
         logo = 'logosmall.png'
+        deluxetablesty = 'deluxetable.sty'
         
         os.system('ln -s %s' % os.path.join(visondata.__path__[0],EuclidViscls))
         os.system('ln -s %s' % os.path.join(visondata.__path__[0],logo))
+        os.system('ln -s %s' % os.path.join(visondata.__path__[0],deluxetablesty))
         
         execline1 = 'latex %s.tex' % fileroot
         os.system(execline1)
@@ -209,5 +297,6 @@ class Report(object):
               
         os.system('rm %s' % EuclidViscls)
         os.system('rm %s' % logo)
-
+        os.system('rm %s' % deluxetablesty)
+        
         return outfiles
