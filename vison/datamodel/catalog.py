1f870fe98ebb9cc6e2ba0b6422d500fb8bc83c87
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2017-05-04 20:40:41 +0100

    work in datamodel/catalog.py, cleaning up minor edits

--- vison/datamodel/catalog.py
+++ vison/datamodel/catalog.py
@@ -6,3 +6,42 @@ Created on Tue May  2 17:25:21 2017
 
 """
 
+
+
+
+class Catalog(dict):
+    """Catalog Class of vison.
+    
+    
+    """
+    
+    def burst(self,inputs,indexes=None):
+        """ """
+        
+    def insert(self,inputs,indexes=None):
+        """ """
+    
+    
+    def zip(self,f,inputs,outputs,indexes,**kwargs):
+        """ """
+        
+        self.insert(outputs,indexes) = f(self.burst(inputs,indexes),**kwargs)
+        
+    
+def proof_of_concept():
+    """ """
+    
+    C = Catalog()
+    C.add_column('Fluence',dimensions=['CCD','Q','Spot'])
+    C.add_column('Exptime',dimensions=['CCD'])
+    
+    def get_flux(fluence,exptime):
+        return fluence/exptime
+    
+    C.zip(get_flux,inputs=['Fluence','Exptime'],outputs=['Flux'])
+    
+    
+if __name__ == '__main__':
+    
+    proof_of_concept()
+    
