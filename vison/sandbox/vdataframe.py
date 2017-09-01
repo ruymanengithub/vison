fb97609e92737637e24b0278d060efcee296d263
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2017-09-01 19:20:22 +0100

    sandbox: vdataframe.py

---
+++ vison/sandbox/vdataframe.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+
+Tests:
+    
+    using multi-indexed pandas data-frames to store Data.
+
+Objectives:
+    
+    - create a data structure with metadata
+
+
+Created on Fri Sep  1 18:36:48 2017
+
+:author: Ruyman Azzollini
+:contact: r.azzollini_at_ucl.ac.uk
+
+"""
+
+
+
+# IMPORT STUFF
+from pdb import set_trace as stop
+import numpy as np
+import pandas as pd
+import datetime
+from vison.pipe import lib as pilib
+from vison import data
+
+# END IMPORT
+
+CCDs = [1,2,3]
+Quads = pilib.Quads
+
+def run():
+    """ """
+    explogf = '../data/EXP_LOG_050800.txt'
+    
+    
+    
+    
+
+if __name__ == '__main__':
+    
+    run()
