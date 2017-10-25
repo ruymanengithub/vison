c288a84799f200ab12dcdaa5ed216a122402da32
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2017-10-25 10:54:25 +0100

    progress in datamodel/core.py

--- vison/datamodel/core.py
+++ vison/datamodel/core.py
@@ -3,7 +3,7 @@
 """
 
 DataDict Class : holds data and results across sub-tasks of a "task" (Test).
-This is the core data-structure used to do analysis and report results.
+This is the CORE data-structure used to do analysis and report results.
 
 
 Created on Thu Sep 21 16:47:09 2017
@@ -22,6 +22,7 @@ from copy import deepcopy
 import string as st
 import astropy as ast
 import itertools
+import tempfile
 
 from vison.pipe import lib as pilib
 from vison.support.files import cPickleDumpDictionary,cPickleRead
@@ -29,7 +30,7 @@ from vison.support.files import cPickleDumpDictionary,cPickleRead
 
 
 
-class Index(object):
+class vIndex(object):
     """ """
     
     def __init__(self,name,vals=[],N=0):
@@ -44,15 +45,19 @@ class Index(object):
         else:
             raise TypeError
     def __str__(self):
-        return '"%s": %i' % (self.name,self.len)
+        return '("%s": %i)' % (self.name,self.len)
+    
+    def __repr__(self):
+        return '("%s": %i)' % (self.name,self.len)
 
 
 
-class MultiIndex(list,object):
+class vMultiIndex(list,object):
     
     def __init__(self,IndexList=[]):
         
-        #assert (isinstance(IndexList,list) or isinstance(IndexList,MultiIndex))
+        assert (isinstance(IndexList,list) or isinstance(IndexList,vMultiIndex) or \
+             (isinstance(IndexList,vIndex)))
         
         try: self += IndexList
         except TypeError: self += [IndexList]
@@ -72,7 +77,6 @@ class MultiIndex(list,object):
             shape.append(item.len)
         return tuple(shape)
     
-    
     def update_names(self):
         self.names = self.get_names()
         
@@ -80,34 +84,30 @@ class MultiIndex(list,object):
         self.shape = self.get_shape()
     
     def append(self,*args):
-        super(MultiIndex,self).append(*args)
+        super(vMultiIndex,self).append(*args)
         self.update_names()
         self.update_shape()
     
     def __add__(self,*args):
-        super(MultiIndex,self).__add__(*args)
+        super(vMultiIndex,self).__add__(*args)
         self.update_names()
-        self.update_shape()
-        
+        self.update_shape()        
     
     def pop(self,*args):
-        super(MultiIndex,self).pop(*args)
+        super(vMultiIndex,self).pop(*args)
         self.update_names()
         self.update_shape()
     
     
     def __getslice__(self,i,j):
-        return MultiIndex(super(MultiIndex,self).__getslice__(i,j))
-    
-    #def __getitem__(self,key):
-    #    return MultiIndex(super(MultiIndex,self).__getitem__(key))
-    
+        return vMultiIndex(super(vMultiIndex,self).__getslice__(i,j))
+        
     def __str__(self):
         inside = st.join(['%s' % item.__str__() for item in self],',')
         return '[%s]' % inside
         
     
-class Column(object):
+class vColumn(object):
     """ """
     
     def __init__(self,array,name,indices):
@@ -115,9 +115,9 @@ class Column(object):
         
         self.array = array.copy()
         self.shape = self.array.shape
-        assert ((isinstance(indices,list)) or (isinstance(indices,MultiIndex)))
+        assert ((isinstance(indices,list)) or (isinstance(indices,vMultiIndex)))
         for index in indices:
-            assert isinstance(index,Index)
+            assert isinstance(index,vIndex)
         
         assert len(self.shape) == len(indices)
         
@@ -125,7 +125,7 @@ class Column(object):
             assert self.shape[i] == indices[i].len, stop()
             
         if isinstance(indices,list):
-            self.indices = MultiIndex(indices)
+            self.indices = vMultiIndex(indices)
         else:
             self.indices = indices
         self.name = name
@@ -133,10 +133,24 @@ class Column(object):
     def name_indices(self):
         """ """
         print self.indices.names
-
+        
+    
+    def __repr__(self):
+        return self.array.__repr__()
     
     def __call__(self):
         return self.array
+    
+    def __getslice__(self,i,j):
+        return self.array.__getslice__(i,j)
+    
+    def __setslice__(self,i,j,y):
+        return self.array.__setslice__(i,j,y)
+    
+    def __setitem__(self,i,y):
+        return self.array.__setitem__(i,y)
+    def __getitem__(self,i):
+        return self.array.__getitem__(i)
     
     def __str__(self):
         return '%s: %s' % (self.name,self.array.__str__())
@@ -152,7 +166,7 @@ class DataDict(object):
         self.meta = meta
         self.mx = OrderedDict()
         self.colnames = []
-        self.indices = MultiIndex()
+        self.indices = vMultiIndex()
         self.products = dict() # data products
         
     def loadExpLog(self,explog):
@@ -166,8 +180,8 @@ class DataDict(object):
         
         ccdcol = explog['CCD']
         
-        ObsIndex = Index('ix',N=Nobs)
-        commIndices = MultiIndex([ObsIndex,Index('CCD',CCDs)])
+        ObsIndex = vIndex('ix',N=Nobs)
+        commIndices = vMultiIndex([ObsIndex,vIndex('CCD',CCDs)])
         
         self.addColumn(uObsID,'ObsID',[ObsIndex])
         
@@ -189,7 +203,7 @@ class DataDict(object):
     def initColumn(self,name,indices,dtype='float32',valini=0.):
         """ """
         
-        assert isinstance(indices,MultiIndex)
+        assert isinstance(indices,vMultiIndex)
         
         shape = indices.shape
         array = np.zeros(shape,dtype=dtype) + valini
@@ -200,7 +214,7 @@ class DataDict(object):
     def addColumn(self,array,name,indices):
         """ """
         
-        column = Column(array,name,indices)
+        column = vColumn(array,name,indices)
         
         self.mx[column.name] = column
         self.colnames.append(column.name)
@@ -273,15 +287,17 @@ class DataDict(object):
                 
                 for i in range(len(prod_vals)):
                     suf = tmp_suf % prod_vals[i]
-                    key = col+suf
+                    key = col + suf
                     jxx = [tuple([jx] + list(prod_ixix[i])) for jx in range(nix0)]
                     
-                    carray = np.array([self.mx[col]()[_jxx] for _jxx in jxx])
+                    carray = np.array([self.mx[col][_jxx] for _jxx in jxx])
                 
                     t[key] = ast.table.Column(carray)
                 
         t.write(outfile,format=format)
         
+    def __cmp__(self,other):
+        return self.__dict__ == other.__dict__
     
     
 def useCases():
@@ -307,6 +323,7 @@ def useCases():
     
     dd.loadExpLog(explog)
     
+    
     print 'dd.indices'
     print dd.indices
     
@@ -317,7 +334,7 @@ def useCases():
     
     
     Xindices = deepcopy(dd.indices)
-    Xindices.append(Index('Quad',vals=['E','F','G','H']))
+    Xindices.append(vIndex('Quad',vals=['E','F','G','H']))
     
     ArrShape = []
     for index in Xindices: ArrShape.append(index.len)
@@ -353,9 +370,9 @@ def useCases():
     OCindices = Xindices[0:2]
     
     dd.initColumn('gain',OCindices,dtype='float32',valini=1.)
-    dd.mx['gain']()[:,0] = 1.
-    dd.mx['gain']()[:,1] = 2.
-    dd.mx['gain']()[:,2] = 3.
+    dd.mx['gain'][:,0] = 1.
+    dd.mx['gain'][:,1] = 2.
+    dd.mx['gain'][:,2] = 3.
     
     
     dd.initColumn('spot_fwhm',OCQindices,dtype='float32',valini=0.)
@@ -369,26 +386,45 @@ def useCases():
         for jCCD in range(nCCD):
             for kQ in range(nQuad):
                 
-                dd.mx['spot_fwhm']()[iObs,jCCD,kQ] = (dd.mx['ObsID']()[iObs] + \
-                    dd.mx['gain']()[iObs,jCCD]*1.e4) * (-1.)**kQ
+                dd.mx['spot_fwhm'][iObs,jCCD,kQ] = (dd.mx['ObsID'][iObs] + \
+                    dd.mx['gain'][iObs,jCCD]*1.e4) * (-1.)**kQ
+    
+    # SAVING
+    cwd = os.path.abspath(os.path.curdir)
     
     # PICKLING
     
-    pickf = 'test_DD.pick'
-    data = dict(dd=dd)
-    cPickleDumpDictionary(data,output=pickf)
+    f1= tempfile.NamedTemporaryFile(mode='w+b',prefix='tmp',dir=cwd,delete=True)
     
-    res = cPickleRead(pickf)
+    with f1:
+    
+        pickf = f1.name
+    
+        data = dict(dd=dd)
+        cPickleDumpDictionary(data,output=pickf)
+        
+        res = cPickleRead(pickf)
+        
+        f1.close()
+    
     
     ddret = res['dd']
+    
+    print 'Pickled == Original : %s' % (dd == ddret,)
     
     
     outf = 'test_DD.txt'
     
-    dd.saveToFile(outf,format='ascii')
+    f2 = tempfile.NamedTemporaryFile(mode='w+b',prefix='tmp',dir=cwd,
+                                     delete=True)
+    with f2:
+        try: 
+            dd.saveToFile(outf,format='ascii')
+            print 'saved catalog to temp file: %s' % f2.name
+            print 'erased %s...' % f2.name
+        except:
+            print 'failed to save catalog to temp file'
     
-    
-    stop()
 
 
 if __name__ == '__main__':
