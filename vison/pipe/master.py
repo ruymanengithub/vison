ed6fc75156b9252df98bcce160c0aff22913516c
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2018-02-02 14:00:25 +0000

    added tests PTC02_800 and PTC02_0, to cover needs of commissioning. Just adding a wavelength in Test_dict at master, really

--- vison/pipe/master.py
+++ vison/pipe/master.py
@@ -88,7 +88,7 @@ class Pipe(object):
     
     for wave in [0,590,640,730,880]:
         Test_dict['FLAT02_%i' % wave] = FLAT0X
-    for wave in [590,640,730,880]:
+    for wave in [590,640,730,800,880,0]:
         Test_dict['PTC02_%i' % wave] = PTC0X
     for temp in [150,156]:
         Test_dict['PTC02_%iK' % temp] = PTC0X
