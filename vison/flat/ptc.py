972ec399221d30423cd90ed806b5566eafed0fd1
ruymanengithub <ruyman.azzollini@gmail.com>
2018-04-18 17:28:17 +0100

    minor changes to flat/ptc.py

--- vison/flat/ptc.py
+++ vison/flat/ptc.py
@@ -20,7 +20,7 @@ import numpy as np
 
 
 def fitPTC(means, var):
-    """ """
+    """Fits Photon Transfer Curve to obtain gain."""
 
     order = np.argsort(means)
     means = np.array(means)[order]
@@ -28,31 +28,27 @@ def fitPTC(means, var):
 
     ixmaxvar = np.argmax(var)
     maxvar = var[ixmaxvar]
-
     ixsel = np.where((means < means[ixmaxvar]) & (var < maxvar*0.95))
-    fvar = var[ixsel]
-    fmeans = means[ixsel]
 
-    res = np.polyfit(fmeans, fvar, 2, full=False, cov=True)
+    res = np.polyfit(means[ixsel], var[ixsel], 2, full=False, cov=True)
 
     p = res[0]
     V = res[1]
     ep = np.sqrt(np.diag(V))
-
-    g = 1./p[1]
-    cuadterm = p[0]
-    rn = p[2]
-
+    
     # Bad results flagging MISSING!
     quality = 0
 
-    fitresults = dict(fit=p, efit=ep, gain=g,
-                      cuadterm=cuadterm, rn=rn, quality=quality)
+    fitresults = dict(fit=p, efit=ep, gain=1./p[1],
+                      cuadterm=p[0], rn=p[2], quality=quality)
 
     return fitresults
 
 
 def foo_bloom(means, var):
-    """DUMMY function (PLACE-HOLDER)"""
+    """DUMMY function (PLACE-HOLDER)
+    (Will) Finds blooming limit (where variance drops, if it does...).
+    
+    """
     res = dict(bloom=np.nan)
     return res
