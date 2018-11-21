8c55d73abc2272343ae15fc65ad3a3965f250357
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2018-11-21 17:25:18 +0000

    added in master.Pipe links to tests FLATFLUX00_2000 and PSFLUX00_2000

--- vison/pipe/master.py
+++ vison/pipe/master.py
@@ -111,9 +111,9 @@ class Pipe(object):
         Test_dict['PSF01_%i' % wave] = PSF0X
     for temp in [150, 156]:
         Test_dict['PSF02_%iK' % temp] = PSF0X
-    for wave in [590, 640, 730, 800, 880, 0]:
+    for wave in [590, 640, 2000, 730, 800, 880, 0]:
         Test_dict['PSFLUX00_%i' % wave] = PSF0X
-    for wave in [590, 640, 730, 800, 880, 0]:
+    for wave in [590, 640, 2000, 730, 800, 880, 0]:
         Test_dict['FLATFLUX00_%i' % wave] = PTC0X
 
     def __init__(self, inputdict, dolog=True, drill=False, debug=False, startobsid=0,
