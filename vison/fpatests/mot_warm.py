c749a8e4c93c43ba6089bf0166f64e14979f501e
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2019-08-29 17:01:41 +0100

    added vison/fpatests/mot_warm

---
+++ vison/fpatests/mot_warm.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 29 16:28:00 2019
+
+@author: raf
+"""
+
+# IMPORT STUFF
+from pdb import set_trace as stop
+import copy
+import numpy as np
+from collections import OrderedDict
+import string as st
+import os
+
+from vison.support import files
+from vison.fpa import fpa as fpamod
+
+from vison.fpatests.metacal import MetaCal
+from vison.plot import plots_fpa as plfpa
+
+from vison.support import vcal
+from vison.datamodel import core as vcore
+from vison.ogse import ogse
+
+
+
+from matplotlib import pyplot as plt
+plt.switch_backend('TkAgg')
+from matplotlib.colors import Normalize
+# END IMPORT
+
+cols2keep = ['test', 'sn_ccd1', 'sn_ccd2', 'sn_ccd3', 'sn_roe', 'sn_rpsu', 'exptime', 'vstart', 'vend', 'rdmode', 'flushes', 'siflsh', 'siflsh_p', 'swellw', 'swelldly', 'inisweep', 
+         'cdpu_clk', 'chinj', 'chinj_on', 'chinj_of', 'id_wid', 'id_dly', 'chin_dly', 'v_tpump', 's_tpump', 'v_tp_mod', 's_tp_mod', 'v_tp_cnt', 's_tp_cnt', 'dwell_v', 'dwell_s', 'toi_fl', 'toi_tp', 
+         'toi_ro', 'toi_ch', 'motr', 'motr_cnt', 'motr_siz', 'source', 'wave', 'mirr_on', 'mirr_pos', 'R1C1_TT', 'R1C1_TB', 'R1C2_TT', 'R1C2_TB', 'R1C3_TT', 'R1C3_TB', 'IDL', 'IDH', 'IG1_1_T', 'IG1_2_T', 
+         'IG1_3_T', 'IG1_1_B', 'IG1_2_B', 'IG1_3_B', 'IG2_T', 'IG2_B', 'OD_1_T', 'OD_2_T', 'OD_3_T', 'OD_1_B', 'OD_2_B', 'OD_3_B', 'RD_T', 'RD_B', 'time', 'HK_CCD1_TEMP_T', 'HK_CCD2_TEMP_T', 
+         'HK_CCD3_TEMP_T', 'HK_CCD1_TEMP_B', 'HK_CCD2_TEMP_B', 'HK_CCD3_TEMP_B', 'HK_CCD1_OD_T', 'HK_CCD2_OD_T', 'HK_CCD3_OD_T', 'HK_CCD1_OD_B', 'HK_CCD2_OD_B', 'HK_CCD3_OD_B', 'HK_COMM_RD_T', 
+         'HK_COMM_RD_B', 'HK_CCD1_IG1_T', 'HK_CCD2_IG1_T', 'HK_CCD3_IG1_T', 'HK_CCD1_IG1_B', 'HK_CCD2_IG1_B', 'HK_CCD3_IG1_B', 'HK_COMM_IG2_T', 'HK_COMM_IG2_B', 'HK_FPGA_BIAS_ID2', 
+         'HK_VID_PCB_TEMP_T', 'HK_VID_PCB_TEMP_B', 'HK_RPSU_TEMP1', 'HK_FPGA_PCB_TEMP_T', 'HK_FPGA_PCB_TEMP_B', 'HK_RPSU_TEMP_2', 'HK_RPSU_28V_PRI_I', 'chk_NPIXOFF', 'chk_NPIXSAT',
+         'offset_pre','offset_img','offset_ove','std_pre','std_img','std_ove']
+
+class MetaMOT(MetaCal):
+    """ """
+    
+    def __init__(self, **kwargs):
+        """ """
+        
+        super(MetaMOT,self).__init__(**kwargs)
+        
+        self.testnames = ['MOT_WARM']
+        self.incols = cols2keep
+        self.ParsedTable = OrderedDict()
+        
+        allgains = files.cPickleRead(kwargs['cdps']['gain'])
+        
+        self.cdps['GAIN'] = OrderedDict()
+        for block in self.blocks:
+            self.cdps['GAIN'][block] = allgains[block]['PTC01'].copy() 
+        
+    
+    def parse_single_test(self, jrep, block, testname, inventoryitem):
+        """ """
+        
+        
+        NCCDs = len(self.CCDs)
+        NQuads = len(self.Quads)
+        session = inventoryitem['session']
+        
+        CCDkeys = ['CCD%i' % CCD for CCD in self.CCDs]
+        
+        IndexS = vcore.vMultiIndex([vcore.vIndex('ix',vals=[0])])
+        
+        
+        IndexCQ = vcore.vMultiIndex([vcore.vIndex('ix',vals=[0]),
+                                     vcore.vIndex('CCD',vals=self.CCDs),
+                                     vcore.vIndex('Quad',vals=self.Quads)])
+        
+    
+        #idd = copy.deepcopy(inventoryitem['dd'])
+        sidd = self.parse_single_test_gen(jrep, block, testname, inventoryitem)
+        
+        
+        # TEST SCPECIFIC
+        # TO BE ADDED:            
+        #   OFFSETS: pre, img, ove
+        #   RON: pre, img, ove
+        #   REFERENCES TO PROFILES
+        
+        CHAMBER = sidd.meta['inputs']['CHAMBER']        
+        CHAMBER_key = CHAMBER[0]        
+        chamber_v = np.array([CHAMBER_key])
+        sidd.addColumn(chamber_v, 'CHAMBERKEY', IndexS, ix=0)        
+        
+        
+        block_v = np.array([block])            
+        sidd.addColumn(block_v, 'BLOCK', IndexS, ix=0)
+                
+        test_v = np.array([jrep+1])            
+        sidd.addColumn(test_v, 'REP', IndexS, ix=0)
+        
+        test_v = np.array([session])            
+        sidd.addColumn(test_v, 'SESSION', IndexS, ix=0)
+        
+        test_v = np.array([testname])            
+        sidd.addColumn(test_v, 'TEST', IndexS, ix=0)
+ 
+        
+        tmp_v_CQ = np.zeros((1,NCCDs,NQuads))
+        
+        rwdvs_off_pre_v = tmp_v_CQ.copy()
+        rwdvs_off_img_v = tmp_v_CQ.copy()
+        rwdvs_off_ove_v = tmp_v_CQ.copy()
+        
+        rwdvs_ron_pre_v = tmp_v_CQ.copy()
+        rwdvs_ron_img_v = tmp_v_CQ.copy()
+        rwdvs_ron_ove_v = tmp_v_CQ.copy()
+        
+        stop()
+        
+        for iCCD, CCDk in enumerate(CCDkeys):
+            for kQ, Q in enumerate(self.Quads):
+                
+                rwdvs_off_pre_v[0,iCCD,kQ] = sidd.mx['offset_pre'][0,iCCD,kQ]
+                rwdvs_off_img_v[0,iCCD,kQ] = sidd.mx['offset_img'][0,iCCD,kQ]
+                rwdvs_off_ove_v[0,iCCD,kQ] = sidd.mx['offset_ove'][0,iCCD,kQ]
+                
+                rwdvs_ron_pre_v[0,iCCD,kQ] = sidd.mx['std_pre'][0,iCCD,kQ]
+                rwdvs_ron_img_v[0,iCCD,kQ] = sidd.mx['std_img'][0,iCCD,kQ]
+                rwdvs_ron_ove_v[0,iCCD,kQ] = sidd.mx['std_ove'][0,iCCD,kQ]
+                
+                
+        sidd.addColumn(rwdvs_off_pre_v, 'RWDVS_OFF_PRE', IndexCQ)
+        sidd.addColumn(rwdvs_off_img_v, 'RWDVS_OFF_IMG', IndexCQ)
+        sidd.addColumn(rwdvs_off_ove_v, 'RWDVS_OFF_OVE', IndexCQ)
+        
+        sidd.addColumn(rwdvs_ron_pre_v, 'RWDVS_RON_PRE', IndexCQ)
+        sidd.addColumn(rwdvs_ron_img_v, 'RWDVS_RON_IMG', IndexCQ)
+        sidd.addColumn(rwdvs_ron_ove_v, 'RWDVS_RON_OVE', IndexCQ)
+
+        # flatten sidd to table
+        
+        sit = sidd.flattentoTable()
+        
+        return sit
+
+
+    
+
+    def dump_aggregated_results(self):
+        """ """
+        
+        stop()
+        
+        outpathroot = self.outpath
+        
+        # RON maps (all tests/waves)
+        
+        
+        # RON maps, ADUs
+        for testname in self.testnames:
+            
+            RONADUMAP = self.get_FPAMAP_from_PT(self.ParsedTable[testname], 
+                                                extractor=self._get_extractor_RON_fromPT(units='ADU'))
+        
+            stestname = st.replace(testname,'_','\_')
+            self.plot_SimpleMAP(RONADUMAP,kwargs=dict(
+                    suptitle='%s: RON [ADU]' % stestname))
+        
+        # RON maps, ELECTRONs
+        for testname in self.testnames:
+            
+            RONEMAP = self.get_FPAMAP_from_PT(self.ParsedTable[testname], 
+                                              extractor=self._get_extractor_RON_fromPT(units='E'))
+        
+            stestname = st.replace(testname,'_','\_')
+            self.plot_SimpleMAP(RONEMAP,kwargs=dict(
+                    suptitle='%s: RON [ELECTRONS]' % stestname))
+        
+        # OFFSET maps
+        
+        for testname in self.testnames:
+            
+            OFFMAP = self.get_FPAMAP_from_PT(self.ParsedTable[testname], extractor=self._extract_OFFSET_fromPT)
+        
+            stestname = st.replace(testname,'_','\_')
+            self.plot_SimpleMAP(OFFMAP,kwargs=dict(
+                    suptitle='%s: OFFSET' % stestname))
+        
+        
+        
+
+        
+        
+        
+        
