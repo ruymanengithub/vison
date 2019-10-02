6d4a5b7daacaf4e9bee2e865e5f48da7f5356935
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2019-10-02 17:11:11 +0100

    refactoring pipe/master so Pipe inherits from a more general GenPipe

--- vison/pipe/master.py
+++ vison/pipe/master.py
@@ -64,7 +64,206 @@ defaults = dict(BLOCKID='R00P00CC000000',
 waittime = 120  # seconds
 waitTO = 3600.*4. # seconds
 
-class Pipe(object):
+
+class GenPipe(object):
+    """Master Pipeline Class."""
+    
+    def __init__(self, inputdict, dolog=True, drill=False, debug=False,
+                 cleanafter=False):
+        """ """
+        
+        self.inputs = defaults.copy()
+        self.inputs.update(inputdict)
+        self.tasks = self.inputs['tasks']
+        self.drill = drill
+        self.debug = debug
+        self.cleanafter = cleanafter
+        self.completion = OrderedDict()
+
+        if self.debug:
+            self.ID = 'PipeDebug%s' % self.tag
+        else:
+            self.ID = 'FM%s%s' % (vistime.get_time_tag(),self.tag)  # ID of the analysis "session"
+
+        self.inputs['ID'] = self.ID
+
+        if dolog:
+            self.logf = 'Calib_%s.log' % self.ID
+
+            if os.path.exists(self.logf):
+                os.system('rm %s' % self.logf)
+
+            self.log = lg.setUpLogger(self.logf)
+            self.log.info(['_', 'Starting FM Calib. Pipeline'] +
+                          self._get_log_header())
+        else:
+            self.log = None
+            
+        self.pipe_session = dict(ID=self.ID,
+                               processes=self.processes)
+
+    def _get_log_header(self):
+        log_header = [
+            'Pipeline ID: %s' % self.ID,
+            'vison version: %s\n' % __version__,
+            'Tasks: %s\n' % self.tasks.__repr__()]
+        return log_header
+
+
+    def launchtask(self, taskname):
+        """ """
+
+        taskinputs = self.inputs[taskname]
+
+        extinputs = copy.deepcopy(self.inputs)
+        alltasks = extinputs['tasks']
+        extinputs.pop('tasks')
+        for _taskname in alltasks:
+            extinputs.pop(_taskname)
+        taskinputs.update(extinputs)
+
+        taskinputs.update(self.pipe_session.copy())
+
+        msg = ['\n\nRunning Task: %s\n' % taskname]
+        msg += ['Inputs:\n']
+        for key in taskinputs:
+            msg += ['%s = %s' % (key, str(taskinputs[key]))]
+
+        if self.log is not None:
+            self.log.info(msg)
+        
+        print('Running: %s' % taskname)        
+        
+        taskreport = self.dotask(taskname, taskinputs,
+                                 drill=self.drill, debug=self.debug,
+                                 cleanafter=self.cleanafter)
+        
+        timemsg = '%.1f minutes in running Task: %s' %\
+                          (taskreport['exectime'], taskname)
+        errormsg = 'Task %s exited with Errors: %s' %\
+                          (taskname, taskreport['Errors'])
+        print(timemsg)
+        print(errormsg)
+        
+        if self.log is not None:
+            self.log.info(timemsg)
+            self.log.info(errormsg)
+        
+        self.completion[taskname] = copy.deepcopy(taskreport)
+
+    def run(self, explogf=None, elvis=None):
+        """ """
+
+        tini = datetime.datetime.now()
+
+        tasknames = self.tasks
+        resultsroot = self.inputs['resultsroot']
+        if not os.path.exists(resultsroot):
+            os.system('mkdir %s' % resultsroot)
+
+        if self.log is not None:
+            self.log.info('\n\nResults will be saved in: %s\n' % resultsroot)
+
+        for taskname in tasknames:
+
+            taskinputs = self.inputs[taskname]
+            taskinputs['resultspath'] = os.path.join(
+                resultsroot, taskinputs['resultspath'])
+
+            if explogf is not None:
+                taskinputs['explogf'] = explogf
+            if elvis is not None:
+                taskinputs['elvis'] = elvis
+
+            self.inputs[taskname] = taskinputs
+
+            self.launchtask(taskname)
+            
+            tpart = datetime.datetime.now()
+            partdtm = ((tpart-tini).seconds)/60.
+            
+            partsummary = self.get_execution_summary(exectime=partdtm)
+            if self.log is not None:
+                self.log.info(partsummary)
+
+        tend = datetime.datetime.now()
+        Dtm = ((tend-tini).seconds)/60.
+
+        summary = self.get_execution_summary(exectime=Dtm)
+        if self.log is not None:
+            self.log.info(summary)
+
+    def get_execution_summary(self, exectime=None):
+        """ """
+        summary = ['_', '_', '_', '_',
+                   '######################################################################'] +\
+            self._get_log_header()
+        
+        for task in self.tasks:
+            if task in self.completion:
+                _compl = self.completion[task]
+                summary += ['_', '%s' % task]
+                summary += ['Executed in %.1f minutes' % _compl['exectime']]
+                summary += ['OBSIDs range = [%i, %i]' % _compl['ObsID_range']]
+                summary += ['Raised Flags = %s' % _compl['flags']]
+                if _compl['Errors']:
+                    summary += ['Executed with ERROR(s)']
+        
+        summary += ['_', '_', '_', '_',
+                   '######################################################################']
+        
+        return summary
+    
+    def dotask(self, taskname, inputs, drill=False, debug=False, cleanafter=False):
+        """Generic test master function."""
+        
+        strip_taskname = utils.remove_iter_tag(taskname,Full=False)
+
+        tini = datetime.datetime.now()
+
+        Errors = False
+        
+        try:
+            test = self.get_test(strip_taskname, inputs=inputs, log=self.log, 
+                                 drill=drill, debug=debug, cleanafter=cleanafter)
+            
+            Errors = test()  # test execution
+        except:
+            self.catchtraceback()
+            Errors = True
+            if self.log is not None:
+                self.log.info('TASK "%s@%s" FAILED, QUITTING!' %
+                              (taskname, self.Test_dict[strip_taskname].__module__))
+            else:
+                print 'TASK "%s@%s" FAILED, QUITTING!' % (
+                    taskname, self.Test_dict[taskname].__module__)
+        
+        tend = datetime.datetime.now()
+        dtm = ((tend-tini).seconds)/60.
+        
+        execlog = OrderedDict()
+        execlog['Task'] = taskname
+        execlog['Errors'] = Errors
+        execlog['exectime'] = dtm
+        try:
+            execlog['ObsID_range'] = (test.dd.mx['ObsID'][:].min(),
+                                   test.dd.mx['ObsID'][:].max() )
+        except:
+            execlog['ObsID_range'] = (-1,-1)
+        
+        try:
+            flags = test.dd.flags.getFlagsOnList()
+        except:
+            flags = ['UNKNOWN']
+        execlog['flags'] = flags.__repr__()
+        
+        test = None
+        
+        return execlog
+
+
+
+class Pipe(GenPipe):
     """Master Class of FM-analysis at block-level of assembly."""
     from vison.dark.BIAS0X import BIAS0X
     from vison.dark.DARK01 import DARK01
@@ -166,6 +365,11 @@ class Pipe(object):
                           self._get_log_header())
         else:
             self.log = None
+            
+        self.pipe_session = dict(ID=self.ID,
+                               BLOCKID=self.BLOCKID,
+                               CHAMBER=self.CHAMBER,
+                               processes=self.processes)
 
     def _get_log_header(self):
         log_header = [
@@ -185,112 +389,6 @@ class Pipe(object):
                          cleanafter=cleanafter)
         return test
 
-    def launchtask(self, taskname):
-        """ """
-
-        taskinputs = self.inputs[taskname]
-
-        extinputs = copy.deepcopy(self.inputs)
-        alltasks = extinputs['tasks']
-        extinputs.pop('tasks')
-        for _taskname in alltasks:
-            extinputs.pop(_taskname)
-        taskinputs.update(extinputs)
-
-        taskinputs.update(dict(ID=self.ID,
-                               BLOCKID=self.BLOCKID,
-                               CHAMBER=self.CHAMBER,
-                               processes=self.processes))
-
-        msg = ['\n\nRunning Task: %s\n' % taskname]
-        msg += ['Inputs:\n']
-        for key in taskinputs:
-            msg += ['%s = %s' % (key, str(taskinputs[key]))]
-
-        if self.log is not None:
-            self.log.info(msg)
-        
-        print('Running: %s' % taskname)        
-        
-        taskreport = self.dotask(taskname, taskinputs,
-                                 drill=self.drill, debug=self.debug,
-                                 cleanafter=self.cleanafter)
-        
-        timemsg = '%.1f minutes in running Task: %s' %\
-                          (taskreport['exectime'], taskname)
-        errormsg = 'Task %s exited with Errors: %s' %\
-                          (taskname, taskreport['Errors'])
-        print(timemsg)
-        print(errormsg)
-        
-        if self.log is not None:
-            self.log.info(timemsg)
-            self.log.info(errormsg)
-        
-        self.completion[taskname] = copy.deepcopy(taskreport)
-
-    def run(self, explogf=None, elvis=None):
-        """ """
-
-        tini = datetime.datetime.now()
-
-        tasknames = self.tasks
-        resultsroot = self.inputs['resultsroot']
-        if not os.path.exists(resultsroot):
-            os.system('mkdir %s' % resultsroot)
-
-        if self.log is not None:
-            self.log.info('\n\nResults will be saved in: %s\n' % resultsroot)
-
-        for taskname in tasknames:
-
-            taskinputs = self.inputs[taskname]
-            taskinputs['resultspath'] = os.path.join(
-                resultsroot, taskinputs['resultspath'])
-
-            if explogf is not None:
-                taskinputs['explogf'] = explogf
-            if elvis is not None:
-                taskinputs['elvis'] = elvis
-
-            self.inputs[taskname] = taskinputs
-
-            self.launchtask(taskname)
-            
-            tpart = datetime.datetime.now()
-            partdtm = ((tpart-tini).seconds)/60.
-            
-            partsummary = self.get_execution_summary(exectime=partdtm)
-            if self.log is not None:
-                self.log.info(partsummary)
-
-        tend = datetime.datetime.now()
-        Dtm = ((tend-tini).seconds)/60.
-
-        summary = self.get_execution_summary(exectime=Dtm)
-        if self.log is not None:
-            self.log.info(summary)
-
-    def get_execution_summary(self, exectime=None):
-        """ """
-        summary = ['_', '_', '_', '_',
-                   '######################################################################'] +\
-            self._get_log_header()
-        
-        for task in self.tasks:
-            if task in self.completion:
-                _compl = self.completion[task]
-                summary += ['_', '%s' % task]
-                summary += ['Executed in %.1f minutes' % _compl['exectime']]
-                summary += ['OBSIDs range = [%i, %i]' % _compl['ObsID_range']]
-                summary += ['Raised Flags = %s' % _compl['flags']]
-                if _compl['Errors']:
-                    summary += ['Executed with ERROR(s)']
-        
-        summary += ['_', '_', '_', '_',
-                   '######################################################################']
-        
-        return summary
     
     def wait_and_run(self, dayfolder, elvis=context.elvis):
         """ """
