f7a83920ffe3abab0c7505785abe7b97c922e395
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2019-12-02 14:54:49 +0000

    added option to flag test repors as DRAFT in fpamaster

--- vison/datamodel/inputs.py
+++ vison/datamodel/inputs.py
@@ -42,6 +42,7 @@ CommonTaskInputs = OrderedDict(sorted([
     ('test', ([str], 'Test/Task Name')),
     ('todo_flags', ([dict], 'Sub-Tasks to-do indicator.')),
     ('preprocessing', ([dict, type(None)], 'Pre-processing kwargs dictionary.')),
+    ('doDraftReport', ([bool], 'Do Draft Test Report')),
     #('tag', ([str,type(None)], 'Used to distinguish tests'))
 ]))
 
--- vison/support/latex.py
+++ vison/support/latex.py
@@ -42,9 +42,14 @@ def replace_in_template(texf, values):
     return texList
 
 
-def generate_header(test, model, author, reference='7-XXX', issue=0.0):
+def generate_header(test, model, author, reference='7-XXX', issue=0.0, doDraft=False):
     """ """
-    headertexf = os.path.join(data.__path__[0], 'header_template.tex')
+
+    if doDraft:
+        headertexf = os.path.join(data.__path__[0], 'header_draft_template.tex')    
+    else:
+        headertexf = os.path.join(data.__path__[0], 'header_template.tex')
+    
     headerList = replace_in_template(
         headertexf, (author, model, test, reference, issue))
     return headerList
