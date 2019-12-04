c52c13cb6567693f92c56e2d6ba09c485c573be0
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2019-12-04 16:45:25 +0000

    added colorbar text in plots_fpa.FpaHeatMap

--- vison/plot/plots_fpa.py
+++ vison/plot/plots_fpa.py
@@ -347,8 +347,10 @@ class FpaHeatMap(BasicPlot):
         if self.meta['doColorbar']:
             #cbar_ax = self.fig.add_axes([0.85, 0.15, 0.05, 0.7])
             #plt.colorbar(cax=cbar_ax, mappable=self.mappables[0],orientation='vertical')
-            self.fig.colorbar(self.mappable, ax=self.ax,
+            ax_cbar = self.fig.colorbar(self.mappable, ax=self.ax,
                               orientation='vertical', fraction=.1)
+            if 'ColorbarText' in self.meta.keys():
+                ax_cbar.set_label(self.meta['ColorbarText'])
 
 
 class FpaFindingChart(FpaHeatMap):
