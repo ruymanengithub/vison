3db8b0877f9f5a0272bc24347fc5cd78f829c922
Ruyman Azzollini <ruyman.azzollini@gmail.com>
2020-03-24 17:39:47 +0000

    work in metatests: persistence

---
+++ vison/metatests/persistence.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+
+Created on Mon Tue 24 16:34:00 2020
+
+@author: raf
+"""
+
+# IMPORT STUFF
+from pdb import set_trace as stop
+import copy
+import numpy as np
+from collections import OrderedDict
+import string as st
+import os
+
+from vison.fpa import fpa as fpamod
+
+from vison.metatests.metacal import MetaCal
+from vison.plot import plots_fpa as plfpa
+
+from vison.support import vcal, utils
+from vison.datamodel import core as vcore
+from vison.ogse import ogse
+from vison.support import files
+from vison.datamodel import ccd as ccdmod
+from vison.datamodel import cdp as cdpmod
+
+from matplotlib import pyplot as plt
+plt.switch_backend('TkAgg')
+from matplotlib.colors import Normalize
+# END IMPORT
+
+cols2keep = [
+    'test',
+    'sn_ccd1',
+    'sn_ccd2',
+    'sn_ccd3',
+    'sn_roe',
+    'sn_rpsu',
+    'exptime',
+    'vstart',
+    'vend',
+    'rdmode',
+    'flushes',
+    'siflsh',
+    'siflsh_p',
+    'swellw',
+    'swelldly',
+    'inisweep',
+    'cdpu_clk',
+    'chinj',
+    'chinj_on',
+    'chinj_of',
+    'id_wid',
+    'id_dly',
+    'chin_dly',
+    'v_tpump',
+    's_tpump',
+    'v_tp_mod',
+    's_tp_mod',
+    'v_tp_cnt',
+    's_tp_cnt',
+    'dwell_v',
+    'dwell_s',
+    'toi_fl',
+    'toi_tp',
+    'toi_ro',
+    'toi_ch',
+    'motr',
+    'motr_cnt',
+    'motr_siz',
+    'source',
+    'wave',
+    'mirr_on',
+    'mirr_pos',
+    'R1C1_TT',
+    'R1C1_TB',
+    'R1C2_TT',
+    'R1C2_TB',
+    'R1C3_TT',
+    'R1C3_TB',
+    'IDL',
+    'IDH',
+    'IG1_1_T',
+    'IG1_2_T',
+    'IG1_3_T',
+    'IG1_1_B',
+    'IG1_2_B',
+    'IG1_3_B',
+    'IG2_T',
+    'IG2_B',
+    'OD_1_T',
+    'OD_2_T',
+    'OD_3_T',
+    'OD_1_B',
+    'OD_2_B',
+    'OD_3_B',
+    'RD_T',
+    'RD_B',
+    'time',
+    'HK_CCD1_TEMP_T',
+    'HK_CCD2_TEMP_T',
+    'HK_CCD3_TEMP_T',
+    'HK_CCD1_TEMP_B',
+    'HK_CCD2_TEMP_B',
+    'HK_CCD3_TEMP_B',
+    'HK_CCD1_OD_T',
+    'HK_CCD2_OD_T',
+    'HK_CCD3_OD_T',
+    'HK_CCD1_OD_B',
+    'HK_CCD2_OD_B',
+    'HK_CCD3_OD_B',
+    'HK_COMM_RD_T',
+    'HK_COMM_RD_B',
+    'HK_CCD1_IG1_T',
+    'HK_CCD2_IG1_T',
+    'HK_CCD3_IG1_T',
+    'HK_CCD1_IG1_B',
+    'HK_CCD2_IG1_B',
+    'HK_CCD3_IG1_B',
+    'HK_COMM_IG2_T',
+    'HK_COMM_IG2_B',
+    'HK_FPGA_BIAS_ID2',
+    'HK_VID_PCB_TEMP_T',
+    'HK_VID_PCB_TEMP_B',
+    'HK_RPSU_TEMP1',
+    'HK_FPGA_PCB_TEMP_T',
+    'HK_FPGA_PCB_TEMP_B',
+    'HK_RPSU_TEMP_2',
+    'HK_RPSU_28V_PRI_I']
+
+class MetaPersist(MetaCal):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+
+        super(MetaPersist, self).__init__(*args, **kwargs)
+
+        self.testnames = ['PERSIST01']
+        self.incols = cols2keep
+        self.ParsedTable = OrderedDict()
+        # self.blocks = self.blocks[-2:] # TESTS
+
+        self.products['PERSIST'] = OrderedDict()
+
+        self.init_fignames()
+        #self.init_outcdpnames()
+
+    def parse_single_test(self, jrep, block, testname, inventoryitem):
+        """ """
+
+
+        NCCDs = len(self.CCDs)
+        NQuads = len(self.Quads)
+        session = inventoryitem['session']
+
+        CCDkeys = ['CCD%i' % CCD for CCD in self.CCDs]
+
+        IndexS = vcore.vMultiIndex([vcore.vIndex('ix', vals=[0])])
+
+        # IndexC = vcore.vMultiIndex([vcore.vIndex('ix',vals=[0]),
+        #                             vcore.vIndex('CCD',vals=self.CCDs)])
+
+        IndexCQ = vcore.vMultiIndex([vcore.vIndex('ix', vals=[0]),
+                                     vcore.vIndex('CCD', vals=self.CCDs),
+                                     vcore.vIndex('Quad', vals=self.Quads)])
+
+        #idd = copy.deepcopy(inventoryitem['dd'])
+
+        sidd = self.parse_single_test_gen(jrep, block, testname, inventoryitem)
+
+        # TEST SPECIFIC
+
+
+        CHAMBER = sidd.meta['inputs']['CHAMBER']
+        CHAMBER_key = CHAMBER[0]
+        chamber_v = np.array([CHAMBER_key])
+        sidd.addColumn(chamber_v, 'CHAMBERKEY', IndexS, ix=0)
+
+        block_v = np.array([block])
+        sidd.addColumn(block_v, 'BLOCK', IndexS, ix=0)
+
+        test_v = np.array([jrep + 1])
+        sidd.addColumn(test_v, 'REP', IndexS, ix=0)
+
+        test_v = np.array([session])
+        sidd.addColumn(test_v, 'SESSION', IndexS, ix=0)
+
+        test_v = np.array([testname])
+        sidd.addColumn(test_v, 'TEST', IndexS, ix=0)
+
+
+        productspath = os.path.join(inventoryitem['resroot'], 'products')
+
+        persist_pick = os.path.join(productspath,
+                os.path.split(sidd.products['PERSIST_STATS'])[-1])
+        persist = files.cPickleRead(persist_pick)
+        #stats = copy.deepcopy(metafitcdp['data']['ANALYSIS'])
+
+        persistkey = '%s_%s_%s_%i' % (testname, block, session, jrep + 1)
+        self.products['PERSIST'][persistkey] = copy.deepcopy(persist)
+        persistkey_v = np.array([persistkey])
+        sidd.addColumn(persistkey_v, 'PERSIST', IndexS, ix=0)
+
+        # flatten sidd to table
+
+        sit = sidd.flattentoTable()
+
+        return sit
+
+        # ADDING Nr. pixels masked: DARK, FLAT, MERGEs
+
+        tmp_v_CQ = np.zeros((1, NCCDs, NQuads))
+
+        Ndark_v = tmp_v_CQ.copy()
+        Nflat_v = tmp_v_CQ.copy()
+        Nmerge_v = tmp_v_CQ.copy()
+        Ncols_v = tmp_v_CQ.copy()
+
+        productspath = os.path.join(inventoryitem['resroot'], 'products')
+
+        NPIX_pick = os.path.join(productspath, os.path.split(sidd.products['DEF_TB_CDP'])[-1])
+
+        NPIX_dict = files.cPickleRead(NPIX_pick)['data']['DEFECTS'].copy()
+
+        for iCCD, CCDk in enumerate(CCDkeys):
+            for kQ, Q in enumerate(self.Quads):
+
+                ixsel = np.where((NPIX_dict['CCD'] == (iCCD + 1)) & (NPIX_dict['Q'] == (kQ + 1)))
+
+                Ndark_v[0, iCCD, kQ] = NPIX_dict['N_DARK'].as_matrix()[ixsel][0]
+                Nflat_v[0, iCCD, kQ] = NPIX_dict['N_FLAT'].as_matrix()[ixsel][0]
+                Nmerge_v[0, iCCD, kQ] = NPIX_dict['N_MERGE'].as_matrix()[ixsel][0]
+                Ncols_v[0, iCCD, kQ] = NPIX_dict['NCOLS_MERGE'].as_matrix()[ixsel][0]
+
+        sidd.addColumn(Ndark_v, 'NDARK', IndexCQ)
+        sidd.addColumn(Nflat_v, 'NFLAT', IndexCQ)
+        sidd.addColumn(Nmerge_v, 'NMERGE', IndexCQ)
+        sidd.addColumn(Ncols_v, 'NCOLS', IndexCQ)
+
+        # Maps of bad pixels
+
+        tmp_v_str = np.zeros((1), dtype='S50')
+
+        for maskkey in self.maskkeys:
+
+            mskkey_v = tmp_v_str.copy()
+
+            all_mask_fits = dict()
+
+            for jCCD, CCDk in enumerate(CCDkeys):
+
+                all_mask_fits[CCDk] = os.path.join(
+                    productspath, os.path.split(sidd.products[maskkey][CCDk])[-1])
+
+            _mskkey = '%s_%s_%s_R%i' % (maskkey, block, session, jrep + 1)
+
+            #debug=False
+            #if block == 'GUYE' and maskkey=='MERGE':
+            #    debug=True
+
+            self.products['MASKSCOOS'][_mskkey] = self._extract_badpix_coordinates(
+                all_mask_fits, block) #,debug=debug)
+
+            mskkey_v[0] = _mskkey
+
+            sidd.addColumn(mskkey_v, 'MASKCOOS_%s' % maskkey, IndexS)
+
+        # flatten sidd to table
+
+        sit = sidd.flattentoTable()
+
+        return sit
+
+
+
+
+    def init_fignames(self):
+        """ """
+
+        if not os.path.exists(self.figspath):
+            os.system('mkdir %s' % self.figspath)
+
+
+        #self.figs['BADCOLS_COUNTS_MAP'] = os.path.join(self.figspath,
+        #                                                'BADCOLS_COUNTS_MAP.png')
+
+
+    def dump_aggregated_results(self):
+        """ """
+        
+
+        if self.report is not None:
+            self.report.add_Section(keyword='dump', 
+                Title='Aggregated Results', level=0)
+            
+            self.add_DataAlbaran2Report()
+
+        function, module = utils.get_function_module()
+        CDP_header = self.CDP_header.copy()
+        CDP_header['DATE'] = self.get_time_tag()
