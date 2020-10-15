8554e536af7eca3630c70e5cc4d2c486fbac03dc
ruymanengithub <ruyman.azzollini@gmail.com>
2020-10-15 11:01:20 +0100

    bug

--- vison/flat/nl_ptc.py
+++ vison/flat/nl_ptc.py
@@ -43,16 +43,16 @@ def get_mse_var(samples):
     else:
         flsamples = samples.flatten()
 
-    flsamples = samples.flatten()
+    
     n = flsamples.size
-    try:
+    MSEvar = np.nan
+    if n>3:
         sigma = np.nanstd(flsamples,ddof=1)
         mu4 = stats.moment(flsamples,moment=4)
-    except:
-        stop()
-    gamma2 = mu4 / sigma**4.-3. # excess kurtosis
+        
+        gamma2 = mu4 / sigma**4.-3. # excess kurtosis
     
-    MSEvar = 1./n * (gamma2 + 2*n/(n-1) ) * sigma**4.
+        MSEvar = 1./n * (gamma2 + 2*n/(n-1) ) * sigma**4.
     
     return MSEvar
 
