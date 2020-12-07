8bc93206b2a530c8418afdc39edb05632050c682
ruymanengithub <ruyman.azzollini@gmail.com>
2020-12-07 17:11:55 +0000

    test

--- vison/flat/NL02.py
+++ vison/flat/NL02.py
@@ -439,7 +439,7 @@ class NL02(NL01.NL01):
                 #ijoffset = np.median(self.dd.mx['offset_pre'][:, iCCD, jQ])
                 ijoffset = self.dd.mx['offset_pre'][:, iCCD, jQ]
 
-                spatScale = (1.-spatDist/spatDist.max()*0.20) # 20% spatial drop in flux
+                spatScale = (1.-spatDist/spatDist.max()*0.0) # x% spatial drop in flux
 
                 ijflu = np.zeros((nObs,Nsectors),dtype='float32')
 
